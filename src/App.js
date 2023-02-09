9f4b9d603f609740bc85c7a55b5f8f08e521b554
Qaiser Hussain <hussaqai@corp.contoursoftware.net>
2023-02-09 12:12:22 +0500

    redux boiler plate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useSelector,useDispatch} from 'react-redux'
+import {increment,decrement} from './store/counterSlice';
 function App() {
+  const dispatch = useDispatch()
+  const val = useSelector((state)=>state.counter.value)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    hello redux {val}
+
+    <button onClick={()=>dispatch(increment(6554))}>increment</button>
+    <button onClick={()=>dispatch(decrement())}>Decrement</button>
     </div>
   );
 }
